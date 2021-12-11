929b22e653f7da84fb4672afa160d5abab8047f2
dyabk <dzmitry.yabkevich@outlook.com>
2021-12-11 17:28:03 -0500

    Created the app tracking feedbacks.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = (props) =>
+  <button onClick = {props.handleClick}>{props.text}</button>
+
+const Header = (props) =>
+  <h1>{props.text}</h1>
+
+const Paragraph = (props) =>
+  <p>{props.text} {props.value}</p>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header text="give feedback"/>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Header text="statistics"/>
+        <Paragraph text="good" value={good} />
+        <Paragraph text="neutral" value={neutral} />
+        <Paragraph text="bad" value={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
