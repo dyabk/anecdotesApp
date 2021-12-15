f43654eee5958e51cb906be8312ec5bf555da229
dyabk <dzmitry.yabkevich@outlook.com>
2021-12-15 04:57:31 -0500

    Replaced the app with the anecdote generator

--- src/App.js
+++ src/App.js
@@ -1,86 +1,23 @@
 import React, { useState } from 'react'
 
-const Button = (props) =>
-  <button onClick = {props.handleClick}>{props.text}</button>
-
-const Header = (props) =>
-  <h1>{props.text}</h1>
-
-const StatisticLine = ({text, value}) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-)
-
-const Statistics = ({good, neutral, bad, all, average, positive}) => {
-  
-  if (good < 1 && bad < 1 && neutral < 1) {
-    return (
-      <div>
-        <Header text="statistics" />
-        <p>No feedback given</p>
-      </div>
-    )
-  }
-  
-  return (
-      <div>
-        <Header text="statistics" />
-        <table>
-          <tbody>
-            <StatisticLine text="good" value={good} />
-            <StatisticLine text="neutral" value={neutral} />
-            <StatisticLine text="bad" value={bad} />
-            <StatisticLine text="all" value={all} />
-            <StatisticLine text="average" value={average} />
-            <StatisticLine text="positive" value={positive*100 + " %"} />
-          </tbody>
-        </table>
-      </div>
-    )
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
-  
-  const setAdditional = (offset1, offset2) => {
-    setAll(all + 1)
-    setAverage( ( good - bad + offset1) / (all + 1) )
-    setPositive( (good + offset2) / (all + 1) )
-  }
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
 
-  const changeBad = () => {
-    setBad(bad + 1)
-    setAdditional(-1, 0)
-  }
-
-  const changeGood = () => {
-    setGood(good + 1)
-    setAdditional(1, 1)
-  }
-
-  const changeNeutral = () => {
-    setNeutral(neutral + 1)
-    setAdditional(0, 0)
-  }
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <Header text="give feedback" />
-        <Button handleClick={() => changeGood()} text="good" />
-        <Button handleClick={() => changeNeutral()} text="neutral" />
-        <Button handleClick={() => changeBad()} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
+      {anecdotes[selected]}
     </div>
   )
-
 }
 
 export default App
